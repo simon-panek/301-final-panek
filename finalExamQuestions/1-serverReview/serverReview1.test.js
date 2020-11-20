5fa2234118f280a96678ee65c95f0400fbdb0dbd
Simon Panek <paneks19@gmail.com>
2020-11-19 16:06:23 -0800

    First commit after rebuild

---
+++ finalExamQuestions/1-serverReview/serverReview1.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const request = require('supertest');
+
+/*
+-------------------------------------------------------------------
+Complete the createServer function to include routes as follows:
+  - a GET request to /hello should respond with status of 200 and the text "Hello"
+  - any other route should return status of 404
+
+NOTE: You do not need write the app.listen() code for your server
+      The test do this for you
+-------------------------------------------------------------------
+*/
+
+// Express sever here
+const createServer = () => {
+
+  const express = require('express');
+  const app = express();
+
+  app.get('/hello', (req, res) => {
+    res.status(200).send('Hello');
+  })
+
+  app.get('*', (req, res) => {
+    res.status(404).send('Not found.');
+  })
+
+  return app;
+};
+
+
+///////////////////////////////////////////////////
+// TESTS
+///////////////////////////////////////////////////
+
+describe('Testing challenge', () => {
+
+  let server = createServer();
+
+  it('responds to /hello', function testHello() {
+    return request(server)
+      .get('/hello')
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.text).toEqual('Hello');
+      })
+  });
+
+  it('404 everything else', function testPath() {
+    return request(server)
+      .get('/foo/bar')
+      .then(response => {
+        expect(response.status).toEqual(404);
+      })
+  });
+});
